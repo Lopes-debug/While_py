73d4a0476fea232c90f334f36aa716d7ce09546c
Leandro Lopes <lopes.ctrlf5@gmail.com>
2024-07-17 16:20:19 -0300

    Add files via upload

---
+++ while.py
@@ -0,0 +1,13 @@
+# ##EXERCICIO 1
+
+# carrinho = []
+
+# while True:
+#     produto = input('Digite a porra do produto: ')
+#     if not produto:
+#         break
+#     quantidade = int(input('E a quantidade? '))
+#     adicionar = [produto, quantidade]
+#     carrinho.append(adicionar)
+
+# print(carrinho)
